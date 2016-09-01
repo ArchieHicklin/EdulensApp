dfd889078135f08e42052f20673511e7e374af74
Archie <afphicklin@gmail.com>
2016-09-01 11:38:57 +0800

    First commit
    No Android or iOS version in this branch

---
+++ packages/telescope-theme-sm-lulin/lib/client/scripts/icons_lulin.js
@@ -0,0 +1,7 @@
+// override icons
+
+Telescope.utils.icons.upvote = "heart-o";
+Telescope.utils.icons.downvote = "hidden";
+Telescope.utils.icons.voted = "heart";
+Telescope.utils.icons.open = "chevron-down";
+Telescope.utils.icons.close = "chevron-up";
